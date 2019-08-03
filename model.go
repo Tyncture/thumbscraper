e1d3f3c55e106cc3c0b6b0c77652472345f00ac6
John Su <git@tyncture.dev>
2019-08-03 19:00:09 +0100

    Add *image.Image field to ImageNodeInfo

--- model.go
+++ model.go
@@ -1,5 +1,9 @@
 package thumbscraper
 
+import (
+	"image"
+)
+
 // ImageNode represents information relating to
 // HTML images elements discovered on the requested URLs.
 type ImageNode struct {
@@ -11,10 +15,12 @@ type ImageNode struct {
 
 // ImageNodeInfo represents information relating to
 // image elements discovered on the requested URLs with
-// additional useful information.
+// additional useful information. Image is only populated
+// if KeepImage is set to true in ImageNodeInfo options.
 type ImageNodeInfo struct {
 	ImageNode
 	Format string
 	Height int
 	Width  int
+	Image  *image.Image
 }
